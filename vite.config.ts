7db3fc7d5f258afe4bf10554fa58909646440df3
mr-react7 <mr007react@gmail.com>
2026-06-15 13:57:25 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // nitro: true,
+  nitro: true,
   tanstackStart: {
     server: {
       entry: "server",
